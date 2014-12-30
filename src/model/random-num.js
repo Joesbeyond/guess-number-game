d631e06d7319aaa7e77a1e097910b8120332c35e
joesbeyond <joesbeyond@gmail.com>
2014-12-30 10:40:55 +0800

    change getRandomNum() method

--- src/model/random-num.js
+++ src/model/random-num.js
@@ -1,12 +1,25 @@
+var _ =require('lodash');
 function RandomNum(){
 
 };
 
 RandomNum.prototype.getRandomNum = function() {
-  var randomNum="";
+/*  var randomNum="";
   for(var i = 0;i < 4; i++) {
     randomNum += Math.floor(Math.random()*10);
   }
   return randomNum;
+};*/
+  var number = 0;
+  var randomNum = [];
+
+  while (randomNum.length < 4) {
+    number = _.random(0, 9).toString();
+    if (!_.contains(randomNum, number)) {
+      randomNum.push(number);
+    }
+  }
+  return randomNum;
 };
+
 module.exports = RandomNum;
